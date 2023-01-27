94fdb8f4bf68755c7e360a614b63d8006db4e061
NischalChenna <chennanischal5000@gmail.com>
2023-01-27 13:04:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+
+const axios = require("axios");
+
+const getAuthSessionToken = async () => {
+  try {
+    const token = await axios.post(
+      "https://api.sandbox.getknit.dev/auth.createSession",
+      {
+        originOrgId: "Rapido",
+        originOrgName: "Rapido",
+        originUserEmail: "nischal@superbeings.ai",
+        originUserName: "Nischal Chenna",
+      },
+      {
+        headers: {
+          Authorization:
+            "Bearer 09e8b0adebb2df4bfc205be8ffe9e3c24297c37d4e005286b2f3bab773e5e24f",
+        },
+      }
+    );
+    return token.data;
+  } catch (err) {
+    throw new Error("Unable to get a token.");
+  }
+};
+app.get("/getSessionToken", async function (req, res) {
+  try {
+    const token = await getAuthSessionToken();
+    console.log(token);
+    // Do your stuff with the token
+    // ...
+    res.send(JSON.stringify(token));
+  } catch (err) {
+    // Error handling here
+    return res.status(401).send(err.message);
+  }
+  //   res.send(getAuthSessionToken());
+});
+
+app.listen(8081, function () {
+  console.log("Example app listening at http://localhost:8081");
+});
